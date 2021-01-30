b07287637a808b06da5c56bfb4dce6cd29a0249f
Jose Montelongo <montelongoj541@gmail.com>
2021-01-29 18:33:25 -0600

    Finish the dark-mode

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,8 +1,30 @@
-import Head from 'next/head'
-import React from 'react'
-import styles from './Layout.module.css'
-
+import Head from 'next/head';
+import React, {useEffect, useState} from 'react';
+import styles from './Layout.module.css';
+import Brightness6RoundedIcon from '@material-ui/icons/Brightness6Rounded';
+import Link from 'next/link';
 function Layout({children, title="World Ranks"}) {
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme",localStorage.getItem("theme"));
+    setTheme(localStorage.getItem("theme"));
+  }, []);
+  const swithTheme = () => {
+    if (theme === "light") {
+      saveTheme("dark")
+    }else{
+      saveTheme("light")
+    }
+  }
+
+  const saveTheme = (theme) => {
+    setTheme(theme);
+    localStorage.setItem("theme",theme);
+    document.documentElement.setAttribute("data-theme",theme);
+  };
+  
+
+  
     return (
     <div className={styles.container}>
       
@@ -12,12 +34,20 @@ function Layout({children, title="World Ranks"}) {
     
       </Head>
       <header className={styles.header}>
-          <div>
+         <Link href="/">
+         <div>
               <img
                   src="/icons/Logo.svg"
                   alt="Logo"
               />
           </div>
+         </Link>
+
+         <div className={styles.button_container}>
+         <button className={styles.themeSwitcher} onClick={swithTheme}>
+             <Brightness6RoundedIcon/>
+         </button>
+         </div>
       </header>
 
       <main className={styles.main}>
